8dcd92abb195283c4310cd6f0e021706a5f2e4ed
Martin Robinson <martinjrobins@gmail.com>
2018-03-05 16:21:25 +0000

    add noise hyperparameter

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ stddev = [m / 5.0 for m in mean]
 times = np.linspace(0, 1000, 1000)
 
 # prior hyperparameters
+alpha0 = 0
+beta0 = 0
 k_0 = 0
 nu_0 = 1
 mu_0 = np.array(mean)
@@ -40,10 +42,10 @@ for i in range(nexp):
     log_likelihood = pints.UnknownNoiseLogLikelihood(problem)
 
     # Create a new prior
+    large = 1e9
     param_prior = pints.MultivariateNormalLogPrior(
-        mean, 1e9 * np.eye(len(mean)))
-    noise_prior = pints.UniformLogPrior(
-        pints.Boundaries([noise / 2], [noise * 2]))
+        mean, large * np.eye(len(mean)))
+    noise_prior = pints.NormalLogPrior(noise, large)
     log_prior = pints.ComposedLogPrior(param_prior, noise_prior)
 
     # Create a posterior log-likelihood (log(likelihood * prior))
@@ -57,33 +59,39 @@ for i in range(nexp):
 n_burn_in = 1000
 for sample in range(n_burn_in):
     if sample % 10 == 0:
-        print('x', end='')
+        print('x', end='', flush=True)
     # generate samples of hierarchical p1e9 * arams
     for i, (sampler, log_posterior) in enumerate(zip(samplers, log_posteriors)):
         x = sampler.ask()
-        sampler.tell(log_posterior(x))[0:len(mean)]
+        sampler.tell(log_posterior(x))
 
 
 # Run a simple hierarchical gibbs-mcmc routine
-n_samples = 2000
-chain = np.zeros((n_samples, 2 * len(mean)))
-exp_chains = [np.zeros((n_samples, len(mean))) for i in range(nexp)]
+n_samples = 10000
+chain = np.zeros((n_samples, 2 * len(mean) + 1))
+exp_chains = [np.zeros((n_samples, len(mean) + 1)) for i in range(nexp)]
 for sample in range(n_samples):
     if sample % 10 == 0:
-        print('.', end='')
-    # generate samples of hierarchical p1e9 * arams
-    xs = np.zeros((len(mean), nexp))
+        print('.', end='', flush=True)
+
+    # generate samples of lower level samplers
+    xs = np.zeros((len(mean) + 1, nexp))
     for i, (sampler, log_posterior) in enumerate(zip(samplers, log_posteriors)):
         x = sampler.ask()
-        xs[:, i] = sampler.tell(log_posterior(x))[0:len(mean)]
+        xs[:, i] = sampler.tell(log_posterior(x))
         exp_chains[i][sample, :] = xs[:, i]
 
-    xhat = np.mean(xs, axis=1)
+    # sample variance of noise from inverse gamma
+    alpha = alpha0 + nexp / 2
+    beta = beta0 + np.sum(xs[-1, :] * xs[-1, :]) / 2
+    noise_variance_sample = scipy.stats.invgamma.rvs(alpha, scale=beta)
+
+    # sample mean and covariance of other params from a normal inverse wishart
+    xhat = np.mean(xs[0:len(mean), :], axis=1)
     C = np.zeros((len(mean), len(mean)))
-    for x in xs.T:
+    for x in xs[0:len(mean), :].T:
         C += np.outer(x - xhat, x - xhat)
 
-    # sample mean and covariance from a normal inverse wishart
     k = k_0 + nexp
     nu = nu_0 + nexp
     mu = (k_0 * mu_0 + nexp * xhat) / k
@@ -95,8 +103,10 @@ for sample in range(n_samples):
     means_sample = scipy.stats.multivariate_normal.rvs(
         mean=mu, cov=covariance_sample / k)
 
+    # store sample to chain
     chain[sample, 0:len(mean)] = means_sample
-    chain[sample, len(mean):] = np.sqrt(np.diagonal(covariance_sample))
+    chain[sample, len(mean):-1] = np.sqrt(np.diagonal(covariance_sample))
+    chain[sample, -1] = np.sqrt(noise_variance_sample)
 
     # replace proposed values with sampled hierarchical parmas,
     # and update priors
@@ -105,9 +115,13 @@ for sample in range(n_samples):
         i_end = (i + 1) * len(mean)
         log_posterior._log_prior._priors[0]._mean = means_sample
         log_posterior._log_prior._priors[0]._cov = covariance_sample
+        log_posterior._log_prior._priors[1]._sigma = np.sqrt(covariance_sample)
+
+# drop first half of chain
+chain = chain[int(n_samples / 2.0):, :]
 
 # Look at distribution in chain
-pints.plot.pairwise(chain, kde=False, true_values=mean + stddev)
+pints.plot.pairwise(chain, kde=False, true_values=mean + stddev + [noise])
 plt.show()
 pints.plot.trace(chain)
 plt.show()
